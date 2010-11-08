ae7fbb03f312619cbc7ead33c7ef7c4547bb9462
Samvel Khalatian <samvel@fnal.gov>
2010-11-08 17:46:52 -0600

    Cleanup LinkDef
    Remove unnecessary PRAGMAs since all objects and collections are saved
    implicitly in the ROOT file. Only "top" objects should be defined.

--- src/S8EventLinkDef.h
+++ src/S8EventLinkDef.h
@@ -1,12 +1,3 @@
-#include <vector>
-
-#include "interface/S8EventID.h"
-#include "interface/S8GenEvent.h"
-#include "interface/S8GenParticle.h"
-#include "interface/S8Jet.h"
-#include "interface/S8Lepton.h"
-#include "interface/S8PrimaryVertex.h"
-#include "interface/S8Trigger.h"
 #include "interface/S8Event.h"
 #include "interface/S8TreeInfo.h"
 #include "interface/S8TriggerCenter.h"
@@ -18,20 +9,6 @@
 #pragma link off all functions;
 
 #pragma link C++ namespace s8;
-
-#pragma link C++ class s8::EventID+;
-#pragma link C++ class s8::GenEvent+;
-#pragma link C++ class s8::GenParticle+;
-#pragma link C++ class s8::Jet+;
-#pragma link C++ class s8::Lepton+;
-#pragma link C++ class s8::PrimaryVertex+;
-#pragma link C++ class s8::Version+;
-#pragma link C++ class s8::Trigger+;
-
-#pragma link C++ class std::vector<s8::Lepton>;
-#pragma link C++ class std::vector<s8::Jet>;
-#pragma link C++ class std::vector<s8::PrimaryVertex>;
-#pragma link C++ class std::vector<s8::Trigger>;
 
 #pragma link C++ class s8::Event+;
 #pragma link C++ class s8::TreeInfo+;
