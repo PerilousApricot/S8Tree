384a094badcd22a3ced4cafefbc4e7711948efda
Samvel Khalatian <samvel@fnal.gov>
2010-11-15 16:43:33 -0600

    Remove unnecessary includes  in the Dictionary

--- src/S8EventLinkDef.h
+++ src/S8EventLinkDef.h
@@ -12,19 +12,12 @@
 #pragma link C++ namespace s8;
 
 #pragma link C++ class s8::Plug+;
-#pragma link C++ class s8::EventID+;
-#pragma link C++ class s8::GenEvent+;
-#pragma link C++ class s8::GenParticle+;
-#pragma link C++ class s8::Jet+;
-#pragma link C++ class s8::Lepton+;
-#pragma link C++ class s8::PrimaryVertex+;
 #pragma link C++ class s8::Version+;
-#pragma link C++ class s8::Trigger+;
 
-#pragma link C++ class std::vector<s8::Jet>;
-#pragma link C++ class std::vector<s8::Lepton>;
-#pragma link C++ class std::vector<s8::PrimaryVertex>;
-#pragma link C++ class std::vector<s8::Trigger>;
+#pragma link C++ class std::vector<s8::Jet *>;
+#pragma link C++ class std::vector<s8::Lepton *>;
+#pragma link C++ class std::vector<s8::PrimaryVertex *>;
+#pragma link C++ class std::vector<s8::Trigger *>;
 
 #pragma link C++ class s8::Event+;
 #pragma link C++ class s8::TreeInfo+;
