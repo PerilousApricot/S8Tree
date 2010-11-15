4da5ec085089ca245db2363f70681833e9320e1f
Samvel Khalatian <ksamdev@gmail.com>
2010-11-15 14:53:57 -0600

    Use pointers in Lepton to speed up compilation

--- interface/S8Lepton.h
+++ interface/S8Lepton.h
@@ -10,86 +10,49 @@
 #define S8_LEPTON
 
 #include <utility>
+#include <memory>
 
-#include <TLorentzVector.h>
-#include <TVector3.h>
-
-#include "interface/S8GenParticle.h"
+class TLorentzVector;
+class TVector3;
 
 namespace s8
 {
+    class GenParticle;
+
     class Lepton
     {
         public:
             // Value and associated Error pair
             //
-            typedef std::pair<double,double> ImpactParameter;
+            typedef std::pair<double, double> ImpactParameter;
 
             Lepton() throw();
 
-            ImpactParameter &impactParameter();
-            const ImpactParameter &impactParameter() const;
+            // Take care of copying b/c of pointers
+            //
+            Lepton(const Lepton &);
+            Lepton &operator =(const Lepton &);
 
-            TLorentzVector &p4();
-            const TLorentzVector &p4() const;
+            void reset();
 
-            TVector3 &vertex();
-            const TVector3 &vertex() const;
+            ImpactParameter *impactParameter();
+            const ImpactParameter *impactParameter() const;
 
-            GenParticle &genParticle();
-            const GenParticle &genParticle() const;
+            TLorentzVector *p4();
+            const TLorentzVector *p4() const;
+
+            TVector3 *vertex();
+            const TVector3 *vertex() const;
+
+            GenParticle *genParticle();
+            const GenParticle *genParticle() const;
 
         private:
-            ImpactParameter _impactParameter;
-            TLorentzVector  _p4;
-            TVector3        _vertex;
-
-            GenParticle     _genParticle;
+            std::auto_ptr<ImpactParameter> _impactParameter;
+            std::auto_ptr<TLorentzVector>  _p4;
+            std::auto_ptr<TVector3>        _vertex;
+            std::auto_ptr<GenParticle>     _genParticle;
     };
-
-    inline Lepton::Lepton() throw()
-    {
-    }
-
-    inline Lepton::ImpactParameter &Lepton::impactParameter()
-    {
-        return _impactParameter;
-    }
-
-    inline const Lepton::ImpactParameter &Lepton::impactParameter() const
-    {
-        return _impactParameter;
-    }
-
-    inline TLorentzVector &Lepton::p4()
-    {
-        return _p4;
-    }
-
-    inline const TLorentzVector &Lepton::p4() const
-    {
-        return _p4;
-    }
-
-    inline TVector3 &Lepton::vertex()
-    {
-        return _vertex;
-    }
-
-    inline const TVector3 &Lepton::vertex() const
-    {
-        return _vertex;
-    }
-
-    inline GenParticle &Lepton::genParticle()
-    {
-        return _genParticle;
-    }
-
-    inline const GenParticle &Lepton::genParticle() const
-    {
-        return _genParticle;
-    }
 }
 
 #endif
